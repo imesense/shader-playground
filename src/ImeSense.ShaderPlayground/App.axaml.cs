1ceafc9604d32c270056319bf17c63fd3557b4e4
acidicMercury8 <acidicmercury8@outlook.com>
2023-09-21 18:43:56 +0300

    Switch to use dependency injection

--- src/ImeSense.ShaderPlayground/App.axaml.cs
+++ src/ImeSense.ShaderPlayground/App.axaml.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -5,18 +7,34 @@ using Avalonia.Markup.Xaml;
 using ImeSense.ShaderPlayground.ViewModels;
 using ImeSense.ShaderPlayground.Views;
 
+using Microsoft.Extensions.DependencyInjection;
+
 namespace ImeSense.ShaderPlayground;
 
 public partial class App : Application {
-    public override void Initialize() {
-        AvaloniaXamlLoader.Load(this);
+    private readonly IServiceProvider _serviceProvider = null!;
+
+    private static IServiceCollection ConfigureServices() {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<MainViewModel>();
+        services.AddSingleton<MainWindow>();
+
+        return services;
     }
+
+    public App() =>
+        _serviceProvider = ConfigureServices().BuildServiceProvider();
+
+    public override void Initialize() =>
+        AvaloniaXamlLoader.Load(this);
 
     public override void OnFrameworkInitializationCompleted() {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
-            desktop.MainWindow = new MainWindow {
-                DataContext = new MainViewModel(),
-            };
+            desktop.MainWindow = _serviceProvider
+                .GetRequiredService<MainWindow>();
+            desktop.MainWindow.DataContext = _serviceProvider
+                .GetRequiredService<MainViewModel>();
         }
 
         base.OnFrameworkInitializationCompleted();
