3ba1f6ab75bb708259e3584e9b8f5822bb72698b
OldSerpskiStalker <ictopiar@gmail.com>
2023-11-05 15:21:23 +0300

    Add render sources

---
+++ Render/Render.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "resource.h"
+
+#define WIN32_LEAN_AND_MEAN // Excludes rarely used libraries in Windows.h
+
+#include <Windows.h>     // All of the Win32 stuff.
+#include <d3d11_1.h>     // Direct3D library
+//#include <d3d10.h>		// Direct3D library
+#include <dxgi.h>        // DirectX Graphics Infrastructure 
+#include <d3dcompiler.h> // Shader compiler
+#include <DirectXMath.h> // SIMD math library utilizing SSE
